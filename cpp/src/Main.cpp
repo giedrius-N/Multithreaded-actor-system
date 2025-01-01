b5199a248f1dcde261b22b49e09fff20d60f863a
giedrius-N <giedrius.navickas159@gmail.com>
2025-01-01 22:13:37 +0200

    CAF include

--- cpp/src/Main.cpp
+++ cpp/src/Main.cpp
@@ -1,7 +1,38 @@
-#include <iostream>
+#include "caf/actor_ostream.hpp"
+#include "caf/actor_system.hpp"
+#include "caf/caf_main.hpp"
+#include "caf/event_based_actor.hpp"
 
-int main()
+using namespace caf;
+using namespace std::literals;
+
+behavior mirror(event_based_actor* self) 
 {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+  return 
+  {
+    [self](const std::string& what) -> std::string 
+    {
+      self->println("{}", what);
+      return std::string{what.rbegin(), what.rend()};
+    },
+  };
 }
+
+void hello_world(event_based_actor* self, const actor& buddy) 
+{
+  self->mail("Hello World!")
+    .request(buddy, 10s)
+    .then(
+      [self](const std::string& what) 
+      {
+        self->println("{}", what);
+      });
+}
+
+void caf_main(actor_system& sys) 
+{
+  auto mirror_actor = sys.spawn(mirror);
+  sys.spawn(hello_world, mirror_actor);
+}
+
+CAF_MAIN()
